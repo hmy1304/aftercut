4820abfadda98a0a045baf7aa4d68c3ac23fc5c3
unknown <hmy1304@kbu.ac.kr>
2026-04-06 21:14:25 +0900

    update

--- backend/src/main/java/picstory/backend/repository/MemberRepository.java
+++ backend/src/main/java/picstory/backend/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package picstory.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import picstory.backend.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
